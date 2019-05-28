9a16aff390894e984347a88c10629d18bc41b03d
Mathias Scherer <scherer.mat@gmail.com>
2019-05-28 17:29:47 +0200

    adds expenses

---
+++ src/resources/Expenses.ts
@@ -0,0 +1,16 @@
+import OAuth2 from "../libs/OAuth2";
+import BaseCrud from "./BaseCrud";
+import Scopes from "../constants/Scopes";
+import { ExpensesStatic } from "../interfaces/ExpensesStatic";
+
+
+export default class Contacts extends BaseCrud<
+    ExpensesStatic.Expense,
+    ExpensesStatic.Expense,
+    ExpensesStatic.Expense,
+    ExpensesStatic.SearchParameters> {
+
+    constructor(bexioAuth: OAuth2) {
+        super(bexioAuth, '/kb_expense', Scopes.KB_EXPENSE_SHOW, Scopes.KB_EXPENSE_EDIT)
+    }
+}
