01023e1be2d089297b26cb3ffbf54740a8162da3
Mathias Scherer <scherer.mat@gmail.com>
2019-05-31 14:15:02 +0200

    adds bill full response

--- src/interfaces/BillsStatic.ts
+++ src/interfaces/BillsStatic.ts
@@ -16,9 +16,11 @@ export namespace BillsStatic {
     total_gross: string;
     total_net: string;
     total_taxes: string;
+    total_regards_taxes: string;
     total_paid_payments: string;
     total_remaining_payments: string;
     total: string;
+    total_rounding_differnce: number;
     mwst_type: number;
     mwst_is_net: boolean;
     show_position_taxes: boolean;
@@ -30,6 +32,10 @@ export namespace BillsStatic {
     viewed_by_client_at: string;
     updated_at: string;
     taxs: Array<Tax>;
+  }
+
+  export interface BillFull extends Bill {
+    positions: Array<CustomPosition | ArticlePosition>;
   }
 
   export interface Tax {
@@ -53,5 +59,30 @@ export namespace BillsStatic {
     is_valid_to?: string;
     is_valid_until?: string;
     updated_at?: string;
+  }
+
+  export interface Position {
+    id: number;
+    type: "KbPositionCustom" | "KbPositionArticle";
+    amount: string;
+    unit_id?: number;
+    account_id: number;
+    unit_name?: string;
+    tax_id: number;
+    tax_value: string;
+    text: string;
+    unit_price: string;
+    discount_in_percent?: string;
+    position_total: string;
+    pos: string;
+    internal_pos: number;
+    parent_id?: number;
+    is_optional: boolean;
+  }
+
+  export interface CustomPosition extends Position {}
+
+  export interface ArticlePosition extends Position {
+    article_id: number;
   }
 }
