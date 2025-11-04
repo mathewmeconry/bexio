287e95d21f28d1e9ae46016fde35f5dddf705873
michael.schmid <michael.schmid@wir-lernen-weiter.ch>
2025-11-04 11:10:00 +0100

    added revert_issue method to Invoices

--- src/resources/Invoices.ts
+++ src/resources/Invoices.ts
@@ -1,24 +1,40 @@
 import { ContactsStatic } from "./../interfaces/ContactsStatic";
 import BaseCrud from "./BaseCrud";
-import {InvoicesStatic} from "../interfaces/InvoicesStatic";
+import { InvoicesStatic } from "../interfaces/InvoicesStatic";
 
 export default class Invoices extends BaseCrud<
-    InvoicesStatic.Invoice,
-    InvoicesStatic.Invoice,
-    InvoicesStatic.Invoice,
-    InvoicesStatic.InvoiceSearchParameters,
-    InvoicesStatic.InvoiceCreate,
-    InvoicesStatic.InvoiceOverwrite
+  InvoicesStatic.Invoice,
+  InvoicesStatic.Invoice,
+  InvoicesStatic.Invoice,
+  InvoicesStatic.InvoiceSearchParameters,
+  InvoicesStatic.InvoiceCreate,
+  InvoicesStatic.InvoiceOverwrite
 > {
   constructor(apiToken: string) {
     super(apiToken, "/kb_invoice");
   }
-  public async sent(id: number, ressource: Partial<InvoicesStatic.InvoiceSent>): Promise<InvoicesStatic.InvoiceSentAnswer> {
+  public async sent(
+    id: number,
+    ressource: Partial<InvoicesStatic.InvoiceSent>
+  ): Promise<InvoicesStatic.InvoiceSentAnswer> {
     return this.request<InvoicesStatic.InvoiceSentAnswer>(
-        "POST",
-        `${this.apiEndpoint}/${id}/send`,
-        undefined,
-        ressource
+      "POST",
+      `${this.apiEndpoint}/${id}/send`,
+      undefined,
+      ressource
+    );
+  }
+  /**
+   * revert a Invoice issue
+   *
+   * @param {number} id
+   * @returns {Promise<{ success: boolean }>}
+   * @memberof Invoices
+   */
+  public async revertIssue(id: number): Promise<{ success: boolean }> {
+    return this.request<{ success: boolean }>(
+      "POST",
+      `${this.apiEndpoint}/${id.toString()}/revert_issue`
     );
   }
 }
