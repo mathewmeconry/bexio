1ef6aee8aa0887c665e1ecc9711142ae05ac4795
Arnaud Durand <a.durand@tokiwi.ch>
2026-09-15 15:48:58 +0200

    feat: implement position delete methods on Invoices and Orders

--- src/resources/Orders.ts
+++ src/resources/Orders.ts
@@ -1,9 +1,11 @@
 import { OrdersStatic } from "./../interfaces/OrdersStatic";
 import BaseCrud from "./BaseCrud";
 import { PositionsStatic } from "../interfaces/PositionsStatic";
+import { PositionStatic } from "../interfaces/PositionStatic";
 import DefaultPositions from "./DefaultPositions";
 import ItemPositions from "./ItemPositions";
 import TextPositions from "./TextPositions";
+import Positions from "./Positions";
 
 export default class Orders extends BaseCrud<
   OrdersStatic.OrderSmall,
@@ -66,5 +68,75 @@ export default class Orders extends BaseCrud<
     return new TextPositions(this.apiToken, "kb_order", orderId).create(
       position
     );
+  }
+  /**
+   * Delete a default position for an order
+   *
+   * @param {number} orderId
+   * @param {number} positionId
+   * @returns {Promise<boolean>}
+   * @memberof Orders
+   */
+  public async deleteDefaultPosition(
+    orderId: number,
+    positionId: number
+  ): Promise<boolean> {
+    return new DefaultPositions(this.apiToken, "kb_order", orderId).delete(
+      positionId
+    );
+  }
+
+  /**
+   * Delete an item position for an order
+   *
+   * @param {number} orderId
+   * @param {number} positionId
+   * @returns {Promise<boolean>}
+   * @memberof Orders
+   */
+  public async deleteItemPosition(
+    orderId: number,
+    positionId: number
+  ): Promise<boolean> {
+    return new ItemPositions(this.apiToken, "kb_order", orderId).delete(
+      positionId
+    );
+  }
+
+  /**
+   * Delete a text position for an order
+   *
+   * @param {number} orderId
+   * @param {number} positionId
+   * @returns {Promise<boolean>}
+   * @memberof Orders
+   */
+  public async deleteTextPosition(
+    orderId: number,
+    positionId: number
+  ): Promise<boolean> {
+    return new TextPositions(this.apiToken, "kb_order", orderId).delete(
+      positionId
+    );
+  }
+
+  /**
+   * Delete a position for an order, whatever its type
+   *
+   * @param {number} orderId
+   * @param {Pick<PositionStatic.Position, "id" | "type">} position
+   * @returns {Promise<boolean>}
+   * @memberof Orders
+   */
+  public async deletePosition(
+    orderId: number,
+    position: Pick<PositionStatic.Position, "id" | "type">
+  ): Promise<boolean> {
+    return new Positions(
+      this.apiToken,
+      "kb_order",
+      orderId,
+      position.type
+    ).delete(position.id);
   }
 }
